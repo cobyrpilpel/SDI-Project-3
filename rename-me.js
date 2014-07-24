24b2c4a07ee36d210db1e39cef00672451fb8f26
cobyrpilpel <cobyrpilpel@Cobys-MacBook-Pro.local>
2014-07-24 19:44:16 -0400

    Procedure done-ish needs polishing

--- rename-me.js
+++ rename-me.js
@@ -7,11 +7,29 @@
 
 
 var cookingMaterials = confirm("Do you have everything you need to get started?");
+
+
 var ingredientsPrimary = ["2 cans of diced tomatoes in sauce",
     "2 cloves diced garlic",
     "1/2 cup diced onion",
-    "3 teaspoons chopped basil",
     "1 Tablespoon minced garlic"
-var ingredientsSecondary = ["Vodka 1 1/2 cups" , "chicken /(diced)" , 
+var ingredientsSecondary = ["Vodka 1 1/2 cups",  , "chicken cubes" , 
 " 1 pint heavy cream" 
 
+var cooktime = 20;
+var servings = 10;
+var 
+var didTheVodkaBurnOff = true
+var isItSimmering = true
+var isItFinished = true
+
+// Procedure 
+var readyToCook = function (cookingMaterials)
+{var answer = cookingMaterials;
+	if (answer === true )
+	{console.log("We are ready to start. Let's get cooking!");}
+	else {console.log("We are supposed to be making Vodka sauce not Vodka on the rocks. Go get your cooking materials!");}
+}
+
+
+//	
