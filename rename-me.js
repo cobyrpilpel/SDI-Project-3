bda376e34d33a6a8da02f7e4f91b544eb4c4fcee
cobyrpilpel <cobyrpilpel@Cobys-MacBook-Pro.local>
2014-07-24 19:12:23 -0400

    first push
    Getting started with my first VARs

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,17 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Coby Pilpel
+//Project 3
+
+
+
+
+var cookingMaterials = confirm("Do you have everything you need to get started?");
+var ingredientsPrimary = ["2 cans of diced tomatoes in sauce",
+    "2 cloves diced garlic",
+    "1/2 cup diced onion",
+    "3 teaspoons chopped basil",
+    "1 Tablespoon minced garlic"
+var ingredientsSecondary = ["Vodka 1 1/2 cups" , "chicken /(diced)" , 
+" 1 pint heavy cream" 
+
