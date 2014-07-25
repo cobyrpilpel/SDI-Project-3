38532d7aecd3775b881a82e6c7a395d0d99eea00
cobyrpilpel <cobyrpilpel@Cobys-MacBook-Pro.local>
2014-07-24 20:38:07 -0400

    Array done (needs polishing)
    Also corrected a duplicate var declaration. I really like garlic but
    not enough to add it twice. Replaced with basil which was the original
    intended var.

--- rename-me.js
+++ rename-me.js
@@ -12,18 +12,20 @@ var cookingMaterials = confirm("Do you have everything you need to get started?"
 var ingredientsPrimary = ["2 cans of diced tomatoes in sauce",
     "2 cloves diced garlic",
     "1/2 cup diced onion",
-    "1 Tablespoon minced garlic"
+    "1 cup minced basil"];
 var ingredientsSecondary = ["Vodka 1 1/2 cups",  , "chicken cubes" , 
-" 1 pint heavy cream" 
+	" 1 pint heavy cream"];
 
-var cooktime = 20;
+var cookTime = 20;
 var servings = 10;
-var 
+var //need a number variable
+var //need a number variable
 var didTheVodkaBurnOff = true
-var isItSimmering = true
+var panIsHot= true
 var isItFinished = true
 
 // Procedure 
+
 var readyToCook = function (cookingMaterials)
 {var answer = cookingMaterials;
 	if (answer === true )
@@ -31,5 +33,21 @@ var readyToCook = function (cookingMaterials)
 	else {console.log("We are supposed to be making Vodka sauce not Vodka on the rocks. Go get your cooking materials!");}
 }
 
+// Array
 
-//	
+
+var gettingStarted = function (vodkaSauceParts, panIsHot)
+{var vodkaSauceParts = sauceAddIns;
+	var tomatoSauceParts = ingredientsPrimary.pop();
+	console.log("Let's get started before this Vodka sauce ends up being just plain old tomato sauce. The ingredients required are " + ingredientsPrimary + " and also " + ingredientsSecondary + ".");
+	ingredientsSecondary.push("salt and pepper to taste");
+	while (panIsHot ===true && ingredientsSecondary.length < 4)
+	{for (var i = 0; i < ingredientsPrimary.length; i++) 
+	{console.log("Great! Now " +ingredientsPrimary[i] + " is cooking away.");}}
+	console.log("Now that we are cooking we have to be careful. Most of these ingredients burn easily.");
+	return vodkaSauceParts;}
+	
+	
+	
+//
+
